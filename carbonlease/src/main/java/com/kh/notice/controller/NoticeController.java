672446c7fb057ff3f6dcfed8feba79d28d03f9d1
unknown <cjysy0104@gmail.com>
2025-11-17 17:01:38 +0900

    목록조회 구현

--- carbonlease/src/main/java/com/kh/notice/controller/NoticeController.java
+++ carbonlease/src/main/java/com/kh/notice/controller/NoticeController.java
@@ -1,5 +1,34 @@
 package com.kh.notice.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.notice.model.dto.NoticeDTO;
+import com.kh.notice.model.service.NoticeService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@Validated
+@RequestMapping("/notices")
+@RequiredArgsConstructor
 public class NoticeController {
 
+	private final NoticeService noticeService;
+	
+	@GetMapping
+	public ResponseEntity<?> findAll(@RequestParam(name="page", defaultValue = "0")int pageNo){
+		
+		List<NoticeDTO> notices = noticeService.findAll(pageNo);
+		
+		return ResponseEntity.ok(notices);
+	}
 }
--- carbonlease/src/main/java/com/kh/notice/model/service/NoticeServiceImpl.java
+++ carbonlease/src/main/java/com/kh/notice/model/service/NoticeServiceImpl.java
@@ -1,5 +1,32 @@
 package com.kh.notice.model.service;
 
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Service;
+
+import com.kh.notice.model.dao.NoticeMapper;
+import com.kh.notice.model.dto.NoticeDTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class NoticeServiceImpl implements NoticeService{
+
+	private final NoticeMapper noticeMapper;
+	
+	@Override
+	public List<NoticeDTO> findAll(int pageNo) {
+		
+		// 유효성 검사
+		
+		// 조회
+		RowBounds rb = new RowBounds(pageNo * 5, 10);
+		
+		return noticeMapper.findAll(rb);
+	}
 
 }
