4778f278ebed244ef461adbc7e52013a40060557
yoonja486 <yoonja486@gmail.com>
2025-11-18 23:56:01 +0900

    board

--- carbonlease/src/main/java/com/kh/board/model/service/BoardServiceImpl.java
+++ carbonlease/src/main/java/com/kh/board/model/service/BoardServiceImpl.java
@@ -23,21 +23,22 @@ public class BoardServiceImpl implements BoardService {
 		BoardVO board = new BoardVO();
 		int endNumber = 0;
 		int startNumber = 0;
-		
-		if(pageNo <= 1) {
-			pageNo = 1;
-			startNumber = 1;
-			endNumber = startNumber * 10;
-		} else {
-			startNumber = 10 + (pageNo-1);
-			endNumber = pageNo * 10;
-		}
-		board.setStartNumber(startNumber);
-		board.setEndNumber(endNumber);
-		
-		List<BoardDTO> boards = boardMapper.boardReadList(board);
 		int total = boardMapper.boardTotalcount();
 		
+		startNumber = pageNo;
+		if(pageNo > 1) {
+			startNumber = (10 * (pageNo - 1)) + 1;
+		}
+		
+		endNumber = pageNo * 10;
+				
+		board.setStartNumber(startNumber);
+		board.setEndNumber(endNumber);
+		System.out.println("시작 건수 >>  "+startNumber + " :: " + endNumber );
+		List<BoardDTO> boards = boardMapper.boardReadList(board);
+		System.out.println("가져온 건수 : "+boards.size());
+		
+		board.setCurrentPage(pageNo);
 		board.setTotalPages(total);
 		board.setBoardList(boards);
 		return board;
