45a604d4b1f0f37fb0ffb1b305e9dd0fa9526d99
suelina001 <elinaen001@gmail.com>
2025-11-23 21:08:34 +0900

    merge

--- carbonlease/src/main/java/com/kh/configuration/SecurityConfigure.java
+++ carbonlease/src/main/java/com/kh/configuration/SecurityConfigure.java
@@ -40,13 +40,13 @@ public class SecurityConfigure {
 						   .csrf(AbstractHttpConfigurer::disable)
 						   .cors(Customizer.withDefaults())
 						   .authorizeHttpRequests(requests -> {
-							   requests.requestMatchers(HttpMethod.POST, "/members/**","/auth/login", "/login/admin", "/auth/refresh").permitAll();
+							   requests.requestMatchers(HttpMethod.POST, "/members","/auth/login", "/login/admin", "/auth/refresh").permitAll();
 							   requests.requestMatchers(HttpMethod.POST, "/boards", "/activityBoards", "/notices", "/campaigns").authenticated();
-							   requests.requestMatchers(HttpMethod.POST, "/activityBoards/**").authenticated();
 							   requests.requestMatchers(HttpMethod.POST, "/campaigns/*/like").authenticated(); // 좋아요 인증 필요
 							   requests.requestMatchers(HttpMethod.GET,"/members/**", "/boards/**","/activityBoards/**", "/uploads/**", "/notices/**", "/campaigns/**").permitAll();
 							   requests.requestMatchers(HttpMethod.PUT,"/members/**","/boards/**","/activityBoards/**", "/notices/**", "/campaigns/**").authenticated();
 							   requests.requestMatchers(HttpMethod.DELETE,"/members/**","/boards/**","/activityBoards/**", "/notices/**", "/campaigns/**").authenticated();
+							   requests.requestMatchers("/admin/campaigns/categories").permitAll();
 							   requests.requestMatchers("/admin/**").hasRole("ADMIN");
 							//    requests.requestMatchers("/admin/**").permitAll();
 						   })
