d462d9b72cac40a900eb9c47455bdc9d76e3a866
suelina001 <elinaen001@gmail.com>
2025-12-05 06:18:31 +0900

    notnull 수정

---
+++ carbonlease/src/main/java/com/kh/admin/main/model/service/AdminMainApiService.java
@@ -0,0 +1,7 @@
+package com.kh.admin.main.model.service;
+
+// [임시 인터페이스] 실제 구현 필요 시 메서드 추가
+public interface AdminMainApiService {
+    // 예시: 실제 메서드 시그니처를 여기에 추가
+    // Object methodName(Object param);
+}
