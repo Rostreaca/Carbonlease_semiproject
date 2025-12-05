216cb3424dfae341abf0453f2a69fd725f968684
yoonja486 <yoonja486@gmail.com>
2025-12-05 17:42:02 +0900

    board

--- carbonlease/src/main/java/com/kh/admin/main/model/service/AdminMainServiceImpl.java
+++ carbonlease/src/main/java/com/kh/admin/main/model/service/AdminMainServiceImpl.java
@@ -1,16 +1,5 @@
 package com.kh.admin.main.model.service;
 
 public class AdminMainServiceImpl implements AdminMainApiService {
-<<<<<<< HEAD
-		// 임시: 인터페이스 메서드 구현 (오류 방지용)
-		public AdminMainServiceImpl() {}
-
-		// 예시: 실제 인터페이스에 정의된 메서드가 있다면 아래처럼 임시 구현
-		// public ReturnType methodName(ParamType param) {
-		//     return null;
-		// }
-}
-=======
 		
 }
->>>>>>> 1394079fdee1b2262d25a6964693989fdb3c9e00
