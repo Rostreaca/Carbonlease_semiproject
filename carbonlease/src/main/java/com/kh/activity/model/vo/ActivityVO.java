da7460eb539d507e682f777efba8a48f4af3e8e1
Rostreaca <92154258+Rostreaca@users.noreply.github.com>
2025-12-11 15:58:24 +0900

    Clean up ActivityVO by removing Lombok annotations
    Removed unused Lombok annotations from ActivityVO.

--- carbonlease/src/main/java/com/kh/activity/model/vo/ActivityVO.java
+++ carbonlease/src/main/java/com/kh/activity/model/vo/ActivityVO.java
@@ -1,11 +1,5 @@
 package com.kh.activity.model.vo;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 public class ActivityBoard {
 
 }
