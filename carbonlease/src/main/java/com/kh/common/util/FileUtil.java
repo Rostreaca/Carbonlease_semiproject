6ab3d76ca8674c9fafac185239875c9704d938a0
suelina001 <elinaen001@gmail.com>
2026-01-04 03:03:16 +0900

    폴더 추가

--- carbonlease/src/main/java/com/kh/common/util/FileUtil.java
+++ carbonlease/src/main/java/com/kh/common/util/FileUtil.java
@@ -13,8 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.awscore.exception.AwsServiceException;
-import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
@@ -59,28 +57,31 @@ public class FileUtil {
 
     	String origin = file.getOriginalFilename();
         String savedName = changeName(origin);
+
+        String key = folderName + "/" + savedName;
         
         PutObjectRequest request = PutObjectRequest.builder()
         										   .bucket(bucketName)
-        										   .key(savedName)
+        										   .key(key)
         										   .contentType(file.getContentType())
         										   .build();
         
         try {
 			s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
-		} catch (S3Exception e) {
-			e.printStackTrace();
-		} catch (AwsServiceException e) {
-			e.printStackTrace();
-		} catch (SdkClientException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		} catch (S3Exception e) { // S3 서비스 관련 모든 에러를 잡음
+            log.error("AWS S3 서비스 에러: {}", e.awsErrorDetails().errorMessage());
+            throw new RuntimeException("S3 업로드 중 문제가 발생했습니다.", e);
+        } catch (IOException e) { // 파일 읽기 관련 에러를 잡음
+            log.error("파일 읽기 에러: ", e);
+            throw new RuntimeException("파일 시스템 접근에 실패했습니다.", e);
+        } catch (Exception e) { // 그 외 예상치 못한 모든 에러
+            log.error("알 수 없는 에러: ", e);
+            throw new RuntimeException("업로드 실패", e);
+        }
         
         
         
-        String filePath = "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + savedName;
+        String filePath = "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + key;
         
         return filePath;
 
@@ -91,17 +92,17 @@ public class FileUtil {
         if (fileUrl == null || fileUrl.isEmpty()) return;
         
         // URL에서 파일명만 추출
-        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+        String key = fileUrl.substring(fileUrl.indexOf(".com/") + 5);
         
         DeleteObjectRequest request = DeleteObjectRequest.builder()
                                                          .bucket(bucketName)
-                                                         .key(fileName)
+                                                         .key(key)
                                                          .build();
         try {
             s3Client.deleteObject(request);
-            log.info("S3 삭제 완료: {}", fileName);
+            log.info("S3 삭제 완료: {}", key);
         } catch (Exception e) {
-            log.error("S3 삭제 실패: {}", fileName, e);
+            log.error("S3 삭제 실패: {}", key, e);
             throw new RuntimeException("S3 파일 삭제 실패", e);
         }
     }
